8296a84db30c92b788531ad36724d6043d3a1a2a
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-03-06 07:15:39 +0300

    completed

--- lib/song.py
+++ lib/song.py
@@ -10,12 +10,6 @@ class Song:
          Song.add_to_genre(genre)
          Song.add_to_artists_count(artist)
                
-             
-         
-         
-        
-        
-        
     @classmethod
     def add_song_to_count(cls):
         cls.count+=1
@@ -37,19 +31,21 @@ class Song:
                 genre_count[genre] = 1
         return genre_count
     @classmethod
-    def artists_count(cls):
-        artists_count = {}
-        for artist in cls.artists:
-            if artist in artists_count:
-                artists_count[artist] += 1
+    def artist_count(self):
+        artist_count = {}
+        for artist in self.artists:
+            if artist in artist_count:
+                artist_count[artist] += 1
             else:
-                artists_count[artist] = 1
-        return artists_count
+                artist_count[artist] = 1
+        return artist_count
+
             
         
             
       
         
+# Tests
         
 wholeheart=Song("whole heart", "Hill song", "worship") 
 findMe=Song("As you find me", "Hill song", "worship") 
@@ -59,6 +55,6 @@ print(Song.count)
 print(Song.genres)
 print(Song.artists)
 print(Song.genre_count())
-print(Song.artists_count())
+print(Song.artist_count())
 
     
